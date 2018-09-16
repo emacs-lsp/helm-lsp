4f09d354735aaecbbc0ed25adc69b5fe345b4ca8
kyoncho <yyoncho@gmail.com>
2018-09-16 09:56:36 +0300

    Melpa related fixes.

--- helm-lsp.el
+++ helm-lsp.el
@@ -1,9 +1,6 @@
 ;;; helm-lsp.el --- LSP helm integration             -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2018  Ivan
-
-;; Author: Ivan <kyoncho@myoncho>
-;; Keywords:
+;; Copyright (C) 2018  Ivan Yonchovski
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -18,28 +15,35 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+;; Author: Ivan Yonchovski <yyoncho@gmail.com>
+;; Keywords: languages, debug
+;; URL: https://github.com/yyoncho/helm-lsp
+;; Package-Requires: ((emacs "25.1") (dash "2.14.1") (lsp-mode "4.0"))
+;; Version: 0.1
+
 ;;; Commentary:
 
-;;
+;; `helm' for lsp function.
 
 ;;; Code:
 
 (require 'helm)
+(require 'dash)
 (require 'lsp-methods)
+(require 'lsp-common)
 
-(defvar lsp-helm-request-id nil)
-(defvar helm-lsp-result-p nil)
-(defvar helm-lsp-result nil)
+(defvar helm-lsp-symbols-request-id nil)
+(defvar helm-lsp-symbols-result-p nil)
+(defvar helm-lsp-symbols-result nil)
 
 (defun helm-lsp-workspace-symbol-action (candidate)
   "Action for helm workspace symbol.
 CANDIDATE is the selected item in the helm menu."
-  (let* ((location (gethash "location" candidate))
-         (start (gethash "start" (gethash "range" location))))
-    (find-file (lsp--uri-to-path (gethash "uri" location)))
+  (-let* (((&hash "uri" "range" (&hash "line" "character")) (gethash "location" candidate)))
+    (find-file (lsp--uri-to-path uri))
     (goto-char (point-min))
-    (forward-line (gethash "line" start))
-    (forward-char (gethash "character" start))))
+    (forward-line line)
+    (forward-char character)))
 
 (defun helm-lsp-workspace-symbol (arg)
   "Preconfigured `helm' for lsp workspace/symbol.
@@ -52,28 +56,27 @@ When called with prefix ARG the default selection will be symbol at point."
     (helm
      :sources (helm-build-sync-source "Workspace symbol"
                 :candidates (lambda ()
-                              (if helm-lsp-result-p
-                                  helm-lsp-result
-                                (let* ((lsp--cur-workspace workspace)
-                                       (request (lsp-make-request "workspace/symbol"
-                                                                  (list :query helm-pattern)))
-                                       (request-id        (plist-get request :id)))
+                              (if helm-lsp-symbols-result-p
+                                  helm-lsp-symbols-result
+                                (with-lsp-workspace workspace
+                                  (-let (((request &as &plist :id request-id) (lsp-make-request
+                                                                               "workspace/symbol"
+                                                                               (list :query helm-pattern))))
+                                    ;; cancel if there is pending request
+                                    (when helm-lsp-symbols-request-id
+                                      (lsp--cancel-request helm-lsp-symbols-request-id)
+                                      (setq helm-lsp-symbols-request-id nil))
 
-                                  ;; cancel if there is pending request
-                                  (when lsp-helm-request-id
-                                    (lsp--cancel-request lsp-helm-request-id)
-                                    (setq lsp-helm-request-id nil))
-
-                                  (setq lsp-helm-request-id request-id)
-                                  (lsp-send-request-async
-                                   request
-                                   (lambda (candidates)
-                                     (setq lsp-helm-request-id nil)
-                                     (and helm-alive-p
-                                          (let ((helm-lsp-result candidates)
-                                                (helm-lsp-result-p t))
-                                            (helm-update)))))
-                                  nil)))
+                                    (setq helm-lsp-symbols-request-id request-id)
+                                    (lsp-send-request-async
+                                     request
+                                     (lambda (candidates)
+                                       (setq helm-lsp-symbols-request-id nil)
+                                       (and helm-alive-p
+                                            (let ((helm-lsp-symbols-result candidates)
+                                                  (helm-lsp-symbols-result-p t))
+                                              (helm-update)))))
+                                    nil))))
                 :action 'helm-lsp-workspace-symbol-action
                 :volatile t
                 :keymap helm-map
@@ -85,7 +88,6 @@ When called with prefix ARG the default selection will be symbol at point."
                                                                 c)))
                 :candidate-number-limit nil
                 :requires-pattern 1)
-
      :input (when arg (thing-at-point 'symbol)))))
 
 (provide 'helm-lsp)
